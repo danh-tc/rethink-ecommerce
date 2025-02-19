94ce01b05614ca760a5a7ad7c38cb27e9f906be8
DANH Tran Cong <trancongdanh99@gmail.com>
2025-02-19 21:12:20 +0700

    Init backend

---
+++ ecommerce-backend/src/test/java/com/rethinkneverends/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rethinkneverends.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
